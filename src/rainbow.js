94e2cf2ee5962bcaf8b7439159b09d1671c1701d
Buu Nguyen <buunguyen@gmail.com>
2013-08-21 12:11:12 -0500

    + Update shadow properties name & default + Update demo

--- src/rainbow.js
+++ src/rainbow.js
@@ -29,51 +29,48 @@
             };
     }());
 
-    var canvas, progressTimerId, fadeTimeId, currentProgress, showing,
+    var canvas, progressTimerId, fadeTimerId, currentProgress, showing,
         addEvent = function(elem, type, handler) {
             if (elem.addEventListener) elem.addEventListener(type, handler, false)
             else if (elem.attachEvent) elem.attachEvent('on' + type, handler)
             else                       elem['on' + type] = handler
         },
         options = {
-            autoRun       : true,
-            barThickness  : 3,
-            barColors     : {
-                '0'       : 'rgba(26,  188, 156, .7)',
-                '.25'     : 'rgba(52,  152, 219, .7)',
-                '.50'     : 'rgba(241, 196, 15,  .7)',
-                '.75'     : 'rgba(230, 126, 34,  .7)',
-                '1.0'     : 'rgba(211, 84,  0,   .7)'
+            autoRun      : true,
+            barThickness : 3,
+            barColors    : {
+                '0'      : 'rgba(26,  188, 156, .9)',
+                '.25'    : 'rgba(52,  152, 219, .9)',
+                '.50'    : 'rgba(241, 196, 15,  .9)',
+                '.75'    : 'rgba(230, 126, 34,  .9)',
+                '1.0'    : 'rgba(211, 84,  0,   .9)'
             },
-            shadowThickness : 1,
-            shadowColor     : '#bbb'
+            shadowBlur   : 10,
+            shadowColor  : 'rgba(0,   0,   0,   .6)'
         },
         repaint = function() {
             canvas.width = window.innerWidth
-            canvas.height = options.barThickness + options.shadowThickness
+            canvas.height = options.barThickness * 5 // need space for shadow
+
             var ctx = canvas.getContext('2d')
-            if (options.barThickness > 0) {
-                if (options.shadowThickness > 0) {
-                    ctx.shadowColor = options.shadowColor
-                    ctx.shadowBlur = options.shadowThickness
-                    ctx.shadowOffsetY = options.shadowThickness
-                }
-                var lineGradient = ctx.createLinearGradient(0, 0, canvas.width, 0)
-                for (var stop in options.barColors)
-                    lineGradient.addColorStop(stop, options.barColors[stop])
-                ctx.lineWidth = options.barThickness
-                ctx.beginPath()
-                ctx.moveTo(0, options.barThickness/2)
-                ctx.lineTo(Math.ceil(currentProgress * canvas.width), options.barThickness/2)
-                ctx.strokeStyle = lineGradient
-                ctx.stroke()
-            }
+            ctx.shadowBlur = options.shadowBlur
+            ctx.shadowColor = options.shadowColor
+
+            var lineGradient = ctx.createLinearGradient(0, 0, canvas.width, 0)
+            for (var stop in options.barColors)
+                lineGradient.addColorStop(stop, options.barColors[stop])
+            ctx.lineWidth = options.barThickness
+            ctx.beginPath()
+            ctx.moveTo(0, options.barThickness/2)
+            ctx.lineTo(Math.ceil(currentProgress * canvas.width), options.barThickness/2)
+            ctx.strokeStyle = lineGradient
+            ctx.stroke()
         },
         createCanvas = function() {
             canvas = document.createElement('canvas')
             var style = canvas.style
             style.position = 'fixed'
-            style.top = style.left = 0
+            style.top = style.left = style.right = style.margin = style.padding = 0
             style.zIndex = 100001
             style.display = 'none'
             document.body.appendChild(canvas)
@@ -88,8 +85,8 @@
             show: function() {
                 if (showing) return
                 showing = true
-                if (fadeTimeId !== null)
-                    window.cancelAnimationFrame(fadeTimeId) 
+                if (fadeTimerId !== null)
+                    window.cancelAnimationFrame(fadeTimerId) 
                 if (!canvas) createCanvas()
                 canvas.style.opacity = 1
                 canvas.style.display = 'block'
@@ -121,21 +118,19 @@
                 }
                 (function loop() {
                     if (rainbow.progress('+.1') >= 1) {
-                        canvas.style.opacity -= .04
+                        canvas.style.opacity -= .05
                         if (canvas.style.opacity <= 0) {
                             canvas.style.display = 'none'
-                            fadeTimeId = null
+                            fadeTimerId = null
                             return
                         } 
                     }
-                    fadeTimeId = window.requestAnimationFrame(loop)
+                    fadeTimerId = window.requestAnimationFrame(loop)
                 })()
             }
         }
 
-    if (typeof exports === 'object')
-        module.exports = rainbow
-    else if (typeof define === 'function' && define.amd)
+    if (typeof define === 'function' && define.amd)
         define(function() { return rainbow })
     else this.rainbow = rainbow
 }).call(this, window, document)
