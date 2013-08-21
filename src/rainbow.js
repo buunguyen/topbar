78913ac06e5b787db9d9798910f1289996b4fe01
Buu Nguyen <buunguyen@gmail.com>
2013-08-20 23:49:41 -0500

    v0.0.3

--- src/rainbow.js
+++ src/rainbow.js
@@ -1,4 +1,4 @@
-/*! rainbow.js 0.0.2, 2013-08-01
+/*! rainbow.js 0.0.3, 2013-08-20
  *  https://github.com/buunguyen/rainbow.js
  *  Copyright (c) 2013 Buu Nguyen
  *  Licensed under the Apache License, Version 2.0 */
@@ -29,58 +29,44 @@
             };
     }());
 
-    var canvas, timerId, currentProgress, showing,
+    var canvas, progressTimerId, fadeTimeId, currentProgress, showing,
         addEvent = function(elem, type, handler) {
             if (elem.addEventListener) elem.addEventListener(type, handler, false)
             else if (elem.attachEvent) elem.attachEvent('on' + type, handler)
             else                       elem['on' + type] = handler
         },
         options = {
-            lineThickness : 5,
-            circleRadius  : 5,
             autoRun       : true,
-            lineColors    : {
-                '0'       : 'rgba(26,  188, 156, .5)',
-                '.25'     : 'rgba(52,  152, 219, .5)',
-                '.50'     : 'rgba(241, 196, 15,  .5)',
-                '.75'     : 'rgba(230, 126, 34,  .5)',
-                '1.0'     : 'rgba(211, 84,  0,   .5)'
+            barThickness  : 3,
+            barColors     : {
+                '0'       : 'rgba(26,  188, 156, .7)',
+                '.25'     : 'rgba(52,  152, 219, .7)',
+                '.50'     : 'rgba(241, 196, 15,  .7)',
+                '.75'     : 'rgba(230, 126, 34,  .7)',
+                '1.0'     : 'rgba(211, 84,  0,   .7)'
             },
-            circleColors  : {
-                '0'       : 'rgba(230, 126, 34, .9)',
-                '1.0'     : 'rgba(241, 196, 15, .9)'
-            }
+            shadowThickness : 1,
+            shadowColor     : '#bbb'
         },
         repaint = function() {
             canvas.width = window.innerWidth
-            canvas.height = Math.max(options.lineThickness, options.circleRadius * 2)
-            
-            var ctx = canvas.getContext('2d'),
-                center = {
-                    x: Math.ceil(currentProgress * canvas.width),
-                    y: canvas.height / 2
+            canvas.height = options.barThickness + options.shadowThickness
+            var ctx = canvas.getContext('2d')
+            if (options.barThickness > 0) {
+                if (options.shadowThickness > 0) {
+                    ctx.shadowColor = options.shadowColor
+                    ctx.shadowBlur = options.shadowThickness
+                    ctx.shadowOffsetY = options.shadowThickness
                 }
-            ctx.clearRect(0, 0, canvas.width, canvas.height)
-
-            if (options.lineThickness > 0) {
                 var lineGradient = ctx.createLinearGradient(0, 0, canvas.width, 0)
-                for (var stop in options.lineColors)
-                    lineGradient.addColorStop(stop, options.lineColors[stop])
-                ctx.lineWidth = options.lineThickness
+                for (var stop in options.barColors)
+                    lineGradient.addColorStop(stop, options.barColors[stop])
+                ctx.lineWidth = options.barThickness
                 ctx.beginPath()
-                ctx.moveTo(0, center.y)
-                ctx.lineTo(center.x, center.y)
+                ctx.moveTo(0, options.barThickness/2)
+                ctx.lineTo(Math.ceil(currentProgress * canvas.width), options.barThickness/2)
                 ctx.strokeStyle = lineGradient
                 ctx.stroke()
-            }
-
-            if (options.circleRadius > 0) {
-                var circleGradient = ctx.createRadialGradient(center.x, center.y, 0, center.x, center.y, options.circleRadius)
-                for (var stop in options.circleColors)
-                    circleGradient.addColorStop(stop, options.circleColors[stop])
-                ctx.fillStyle = circleGradient
-                ctx.arc(center.x, center.y, options.circleRadius, 0, Math.PI * 2, false)
-                ctx.fill()
             }
         },
         createCanvas = function() {
@@ -91,7 +77,7 @@
             style.zIndex = 100001
             style.display = 'none'
             document.body.appendChild(canvas)
-            addEvent(window, "resize", repaint)
+            addEvent(window, 'resize', repaint)
         },
         rainbow = {
             config: function(opts) {
@@ -102,19 +88,21 @@
             show: function() {
                 if (showing) return
                 showing = true
+                if (fadeTimeId !== null)
+                    window.cancelAnimationFrame(fadeTimeId) 
                 if (!canvas) createCanvas()
-                currentProgress = 0
+                canvas.style.opacity = 1
                 canvas.style.display = 'block'
-                repaint()
+                rainbow.progress(0)
                 if (options.autoRun) {
                     (function loop() {
-                        timerId = window.requestAnimationFrame(loop)
+                        progressTimerId = window.requestAnimationFrame(loop)
                         rainbow.progress('+' + (.05 * Math.pow(1-Math.sqrt(currentProgress), 2)))
                     })()
                 }
             },
             progress: function(to) {
-                if (typeof to === "undefined" || !showing)
+                if (typeof to === "undefined")
                     return currentProgress
                 if (typeof to === "string")
                     to = (to.indexOf('+') !== -1 || to.indexOf('-') !== -1)
@@ -127,11 +115,21 @@
             hide: function() {
                 if (!showing) return
                 showing = false
-                canvas.style.display = 'none'
-                if (timerId != null) {
-                    window.cancelAnimationFrame(timerId)
-                    timerId = null
+                if (progressTimerId != null) {
+                    window.cancelAnimationFrame(progressTimerId)
+                    progressTimerId = null
                 }
+                (function loop() {
+                    if (rainbow.progress('+.1') >= 1) {
+                        canvas.style.opacity -= .04
+                        if (canvas.style.opacity <= 0) {
+                            canvas.style.display = 'none'
+                            fadeTimeId = null
+                            return
+                        } 
+                    }
+                    fadeTimeId = window.requestAnimationFrame(loop)
+                })()
             }
         }
 
